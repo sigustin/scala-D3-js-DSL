89a0bd93f8d37784d6f6299d221bd97c4eff49d5
sigustin <simon.gustin@student.uclouvain.be>
2018-05-07 16:32:57 +0200

    Remove warnings

--- src/main/scala/lib/Graph.scala
+++ src/main/scala/lib/Graph.scala
@@ -1,9 +1,11 @@
 package lib
 
 import d3v4._
+
 import scala.scalajs.js
-import js.Dynamic.{ global => gJS }
+import js.Dynamic.{global => gJS}
 import lib.ImplicitConv._
+import org.scalajs.dom
 
 trait Graph {
     var scale = 0               // power of ten multiplier of the representation of the data hold in data
@@ -13,19 +15,19 @@ trait Graph {
 
 
     var target = "svg" // is a html selector used as destination for the graphe
-    var svg = d3.select(target)
+    var svg: Selection[dom.EventTarget] = d3.select(target)
 
     var data: Option[js.Array[js.Array[Double]]] = None
 
 
 
-    def setDimension(h: Int, w:Int) = {
+    def setDimension(h: Int, w:Int): Graph = {
         heightLocal = Some(h)
         widthLocal = Some(w)
         this
     }
 
-    def setTarget(t: String) = {
+    def setTarget(t: String): Graph = {
         target = t
         svg = d3.select(target)
         this
@@ -45,20 +47,20 @@ trait Graph {
         }
     }
 
-    def setData(d: js.Array[js.Array[Double]]) = {
-        data = Some(trasformeData(d))
+    def setData(d: js.Array[js.Array[Double]]): Graph = {
+        data = Some(transformData(d))
         this
     }
 
-    def setData(d: List[List[Double]]) = {
+    def setData(d: List[List[Double]]): Graph = {
         val tmpD:js.Array[js.Array[Double]] = js.Array()
         d.foreach(tmpD.append(_))
-        data = Some(trasformeData(tmpD))
+        data = Some(transformData(tmpD))
         this
     }
 
 
-    private def trasformeData(d: js.Array[js.Array[Double]]): js.Array[js.Array[Double]] = {
+    private def transformData(d: js.Array[js.Array[Double]]): js.Array[js.Array[Double]] = {
         var maxFigureBehideCommaVal = d.maxNbFigureBehindComma
         scale=maxFigureBehideCommaVal
         (d * (10**maxFigureBehideCommaVal)).round
